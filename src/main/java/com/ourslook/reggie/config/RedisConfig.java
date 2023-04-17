a98e478e440a724ccd99efc46d0eb9820e015cdd
王犇犇 <3028589699@qq.com>
2023-04-17 10:13:37 +0800

    Redis环境的配置

---
+++ src/main/java/com/ourslook/reggie/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.ourslook.reggie.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * ClassName: RedisConfig
+ * Package: com.ourslook.reggie.config
+ * Description:
+ *
+ * @Author 王犇犇
+ * @Create 2023/4/17 10:01
+ * @Version 1.0
+ */
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //默认的Key序列化器为：JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
